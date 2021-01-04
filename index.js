7491129499743001ad63d190514902aa36b6df3b
Brozzor <46285168+Brozzor@users.noreply.github.com>
2021-01-04 16:02:27 +0100

    fix - oublie du index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// Dev comments :
+//
+// All uncommented lines reason (https unactive)
+// ------------------------------------------------------------------------------
+const app = require("express")();
+const http = require("http")
+const server = http.createServer(app);
+const apiRouter = require("./apiRouter").router;
+const bodyparser = require("body-parser");
+
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use("/", apiRouter);
+
+server.listen(process.env.PORT || 8090);
