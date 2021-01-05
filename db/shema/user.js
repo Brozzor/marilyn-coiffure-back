228d4b86e66fe2e99396413ec4d1ed6ca8b17202
Brozzor <46285168+Brozzor@users.noreply.github.com>
2021-01-05 16:52:59 +0100

    modification du model de base de données user

--- db/shema/user.js
+++ db/shema/user.js
@@ -8,13 +8,13 @@ let userSchema = new Schema({
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
     password: { type: String, required: true },
-    address: { type: Object, required: true },
+    address: { type: Object, required: false },
     createDate: { type: Number, required: true },
     tokenSession: { type: String, required: false },
     createIp: { type: String, required: true },
-    lastView: { type: Number, required: false },
     rank: { type: Number, required: false },
-    resetPassword: { type: Object, required: true }
+    resetPassword: { type: Object, required: true },
+    mobile: { type: Number, required: true }
 },{ collection: "user-data" });
 
 module.exports = mongoose.model('user-data', userSchema);    
--- db/type/user.js
+++ db/type/user.js
@@ -1,18 +1,21 @@
 module.exports = class User {
-    constructor(mail, firstname, lastname, password, ip) {
-      this.mail = mail;
-      this.firstname = firstname;
-      this.lastname = lastname;
-      this.password = password;
-      this.createDate = Math.round(+new Date() / 1000);
-      this.tokenSession = null;
-      this.createIp = ip;
-      this.lastView = Math.round(+new Date() / 1000);
-      this.rank = 0;
-      this.resetPassword = {
-          date: null,
-          token: null
-      }
-
-    }
-  };
+  constructor(mail, firstname, lastname, password, ip) {
+    this.mail = mail;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.password = password;
+    this.createDate = Math.round(+new Date() / 1000);
+    this.tokenSession = null;
+    this.createIp = ip;
+    this.rank = 0;
+    this.resetPassword = {
+      date: null,
+      token: null,
+    };
+    this.address = {
+      address: null,
+      city: null,
+      zip: null,
+    };
+  }
+};
