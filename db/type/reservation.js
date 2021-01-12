5b857a49fe7e15a03a7b5eda096e8a0b6763ef3b
bertyn99 <bboulikou@gmail.com>
2021-01-12 15:05:48 +0100

    modif reservation ajout de status,produitderive, montant...

--- db/type/reservation.js
+++ db/type/reservation.js
@@ -2,7 +2,10 @@ module.exports = class Reservation {
     constructor(dateReservation, address) {
         this.dateReservation = dateReservation;
         this.address = address;
-        // status
+        this.status = "";
+        this.produitDerive = [];
+        this.montant = 0;
+        this.payementType = "";
         // tableau vide pour les produits dérivé
         // montant
         // moyen de paiement
