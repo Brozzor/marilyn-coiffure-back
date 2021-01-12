c424482408e75fb62a18f195a574e433c90334b5
Brozzor <46285168+Brozzor@users.noreply.github.com>
2021-01-12 14:24:40 +0100

    fix - les variables d'environnements était mal nommer

--- db/type/reservation.js
+++ db/type/reservation.js
@@ -2,5 +2,9 @@ module.exports = class Reservation {
     constructor(dateReservation, address) {
         this.dateReservation = dateReservation;
         this.address = address;
+        // status
+        // tableau vide pour les produits dérivé
+        // montant
+        // moyen de paiement
     };
 }
