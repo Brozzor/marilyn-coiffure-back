685b1d46fd87b6ac8362f4bc4797d59926d8c5d5
bertyn99 <bboulikou@gmail.com>
2021-01-13 17:33:50 +0100

    ajout de reservation schema

--- db/shema/reservation.js
+++ db/shema/reservation.js
@@ -3,5 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let reservationSchema = new Schema({
-
+    dateReservation: { type: String, required: true },
+    address: { type: String, required: true },
+    status: { type: String, required: true },
+    produitDerive: { type: Array, required: false },
+    montant: { type: Number, required: false },
+    payementType: { type: String, required: false },
 });
