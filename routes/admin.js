7bc4580f80a5ff5de5b9582cdb6d3813ee143da8
bertyn99 <bboulikou@gmail.com>
2021-01-13 17:57:40 +0100

    ajout  route admin

---
+++ routes/admin.js
@@ -0,0 +1,7 @@
+// To declare
+//db and schema
+const User = require("../db/type/user");
+const UserShema = require("../db/shema/user");
+const Reservation = require("../db/type/Reservation");
+const ReservationShema = require("../db/shema/reservation");
+const database = require("../db/connection");
