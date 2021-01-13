0e73261f4e6666d0d5d4eba886bff159a0196775
bertyn99 <bboulikou@gmail.com>
2021-01-13 18:06:32 +0100

    ajout des fonction des route d'admin

--- routes/admin.js
+++ routes/admin.js
@@ -5,3 +5,26 @@ const UserShema = require("../db/shema/user");
 const Reservation = require("../db/type/Reservation");
 const ReservationShema = require("../db/shema/reservation");
 const database = require("../db/connection");
+
+
+//////////////////////////////////////////////////////////////////////////
+//                                Crud Admin                            //
+/////////////////////////////////////////////////////////////////////////
+
+async function bookinglist() {//list des rende-vous à venir
+
+}
+async function balance() {//balance de la société
+
+}
+
+async function addBookingPaid() {//ajout des des reservation payé
+
+}
+
+
+module.exports = {
+    bookinglist,
+    balance,
+    addBookingPaid
+};
