c3c59e337323a97c658942e6ebe2c18ea24d744c
bertyn <bertynboulikou@gmail.com>
2021-01-19 16:14:23 +0100

    retrais des log

--- services/verifyToken.js
+++ services/verifyToken.js
@@ -2,12 +2,11 @@ const UserShema = require("../db/shema/user");
 
 module.exports = async (req, res, next) => {
 
-  const searchBySessionToken = await UserShema.findOne({ tokenSession: req.headers['tokensession']});
+  const searchBySessionToken = await UserShema.findOne({ tokenSession: req.headers['tokensession'] });
   if (searchBySessionToken) {
     req.userByToken = searchBySessionToken;
-   next();
+    next();
   } else {
-    await log.addLog(req,'reconnection.error');
     res.sendStatus(418);
     return false;
   }
